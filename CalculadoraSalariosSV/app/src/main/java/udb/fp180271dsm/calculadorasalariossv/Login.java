c6df62d62e173797d4959ad681e108335af7b690
danielwilfredo <xvwilito11@gmail.com>
2022-04-24 18:03:26 -0600

    Realiza inicio de sesion

--- CalculadoraSalariosSV/app/src/main/java/udb/fp180271dsm/calculadorasalariossv/Login.java
+++ CalculadoraSalariosSV/app/src/main/java/udb/fp180271dsm/calculadorasalariossv/Login.java
@@ -1,20 +1,53 @@
 package udb.fp180271dsm.calculadorasalariossv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
 
 public class Login extends AppCompatActivity {
 
+    private EditText txtCorreo, txtContra;
+    private Button btnLogin;
+
+    private FirebaseAuth mAuth;
+
     TextView txtRegistrarse1, txtRegistrarse2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //empieza codigo para inicio de sesion con firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
+        });
+
+
+
+
+
 
         //region Abrir Registarse
         txtRegistrarse1 = findViewById(R.id.txtRegistrar1);
@@ -34,14 +67,60 @@ public class Login extends AppCompatActivity {
         //endregion
     }
 
+    //funcion para inicio de sesion ocn firebase
+    private void iniciarSesion()
+    {
+        String correo, contrasena;
+        correo = txtCorreo.getText().toString();
+        contrasena = txtContra.getText().toString();
+
+        if (TextUtils.isEmpty(correo)) {
+            Toast.makeText(getApplicationContext(), "Ingrese un Correo...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(contrasena)) {
+            Toast.makeText(getApplicationContext(), "Ingrese una contraseña!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(correo, contrasena)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Inicio de Sesion Correcto!", Toast.LENGTH_LONG).show();
+
+                            //abrimos la pantalla del menu
+                            AbrirActivity(Login.this, ContenedorMenu.class);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Inicio de Sesion Incorrecto", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+    }
+
+
+
+
     public void AbrirActivity(Context ActivityActual, Class ActivityNueva) {
         Intent intento = new Intent(ActivityActual, ActivityNueva);
         startActivity(intento);
     }
 
-    public void IniciarSesion(View v)
+    public void InicioSesionCorrecto(View v)
     {
         AbrirActivity(Login.this, ContenedorMenu.class);
+    }
+
+
+
+    private void initializeUI() {
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtContra = findViewById(R.id.txtContra);
+
+        btnLogin = findViewById(R.id.btnIniciarSesion);
+
     }
 
 }
--- CalculadoraSalariosSV/app/src/main/java/udb/fp180271dsm/calculadorasalariossv/Registrarse.java
+++ CalculadoraSalariosSV/app/src/main/java/udb/fp180271dsm/calculadorasalariossv/Registrarse.java
@@ -6,11 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registrarse extends AppCompatActivity {
 
-    TextView txtIniciarSesion;
+    TextView txtIniciarSesion, txtNombreApellido, txtCorreoRegistro, txtContra, txtConfirmarContra;
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
